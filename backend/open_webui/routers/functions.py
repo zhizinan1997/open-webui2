68129b47d68eeb5591b69c51f9b35a73cdc083a3
U8F69 <orenzhang@ovinc.cn>
2025-05-29 10:47:39 +0800

    fix(functions): reset to official change
    This reverts commit 9e1fb15285376f1a20869b8feef42a959ed75178.

--- backend/open_webui/routers/functions.py
+++ backend/open_webui/routers/functions.py
@@ -358,11 +358,8 @@ async def get_function_valves_spec_by_id(
 ):
     function = Functions.get_function_by_id(id)
     if function:
-        if id in request.app.state.FUNCTIONS:
-            function_module = request.app.state.FUNCTIONS[id]
-        else:
-            function_module, function_type, frontmatter = load_function_module_by_id(id)
-            request.app.state.FUNCTIONS[id] = function_module
+        function_module, function_type, frontmatter = load_function_module_by_id(id)
+        request.app.state.FUNCTIONS[id] = function_module
 
         if hasattr(function_module, "Valves"):
             Valves = function_module.Valves
@@ -386,11 +383,8 @@ async def update_function_valves_by_id(
 ):
     function = Functions.get_function_by_id(id)
     if function:
-        if id in request.app.state.FUNCTIONS:
-            function_module = request.app.state.FUNCTIONS[id]
-        else:
-            function_module, function_type, frontmatter = load_function_module_by_id(id)
-            request.app.state.FUNCTIONS[id] = function_module
+        function_module, function_type, frontmatter = load_function_module_by_id(id)
+        request.app.state.FUNCTIONS[id] = function_module
 
         if hasattr(function_module, "Valves"):
             Valves = function_module.Valves
@@ -449,11 +443,8 @@ async def get_function_user_valves_spec_by_id(
 ):
     function = Functions.get_function_by_id(id)
     if function:
-        if id in request.app.state.FUNCTIONS:
-            function_module = request.app.state.FUNCTIONS[id]
-        else:
-            function_module, function_type, frontmatter = load_function_module_by_id(id)
-            request.app.state.FUNCTIONS[id] = function_module
+        function_module, function_type, frontmatter = load_function_module_by_id(id)
+        request.app.state.FUNCTIONS[id] = function_module
 
         if hasattr(function_module, "UserValves"):
             UserValves = function_module.UserValves
@@ -473,11 +464,8 @@ async def update_function_user_valves_by_id(
     function = Functions.get_function_by_id(id)
 
     if function:
-        if id in request.app.state.FUNCTIONS:
-            function_module = request.app.state.FUNCTIONS[id]
-        else:
-            function_module, function_type, frontmatter = load_function_module_by_id(id)
-            request.app.state.FUNCTIONS[id] = function_module
+        function_module, function_type, frontmatter = load_function_module_by_id(id)
+        request.app.state.FUNCTIONS[id] = function_module
 
         if hasattr(function_module, "UserValves"):
             UserValves = function_module.UserValves
