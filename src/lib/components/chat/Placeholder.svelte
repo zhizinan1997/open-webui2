f772aca4e436fabf77b2a4d5c418cd0c105107f0
U8F69 <orenzhang@ovinc.cn>
2025-06-04 14:05:02 +0800

    fix(model): model logo cors origin invalid

--- src/lib/components/chat/Placeholder.svelte
+++ src/lib/components/chat/Placeholder.svelte
@@ -123,7 +123,6 @@
 									}}
 								>
 									<img
-										crossorigin="anonymous"
 										src={model?.info?.meta?.profile_image_url ??
 											($i18n.language === 'dg-DG'
 												? `/doge.png`
